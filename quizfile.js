1f36ee0d94246da6155db3392ce40e0adc6821e1
vaniairnanda <vaniairnanda@gmail.com>
2019-11-22 18:50:44 +0700

    submit updates

--- quizfile.js
+++ quizfile.js
@@ -11,51 +11,52 @@ function getFormData(){
     
 function catQuiz(){
   var result = []
-  var userInput = ''
+  var userInput = []
   var question1s = document.getElementsByName("question1");
   for (var i = 0; i < question1s.length; i++) {
     if (question1s[i].checked) {
-        userInput[0] = question1s[i].value;
-        result.push(userInput[0])
+        result.push(question1s[i].value)
     }
 }
 var question2s = document.getElementsByName("question2");
 
   for (var j = 0; j < question2s.length; j++) {
     if (question2s[j].checked) {
-        userInput[1] = question2s[j].value;
-        result.push(userInput[1])
+        result.push(question2s[j].value)
     }
 }
 if (result[0] === 'Orange' && result[1] === 'Chonky') {
   var image = document.createElement('img');
   image.setAttribute('src','chonkorange.jpg');
   document.body.appendChild(image);
-  alert `Your cat is ready ${image}`
+  resultsContainer.innerHTML = `Hi, ${getFormData()} Your cat is ready ${image}`
 } else if (result[0] === 'Orange' && result[1] === 'Smol') {
   var image = document.createElement('img');
   image.setAttribute('src','Munchkin-Cat.jpg');
   document.body.appendChild(image);
-  alert `Your cat is ready ${image}`
+  resultsContainer.innerHTML = `Hi, ${getFormData()} Your cat is ready ${image}`
 } else if (result[0] === 'Grey' && result[1] === 'Chonky') {
   var image = document.createElement('img');
   image.setAttribute('src','chonkygrey.jpeg');
   document.body.appendChild(image);
-  alert `Your cat is ready ${image}`
+  resultsContainer.innerHTML = `Hi, ${getFormData()} Your cat is ready ${image}`
 } else if (result[0] === 'Grey' && result[1] === 'Smol') {
   var image = document.createElement('img');
-  image.setAttribute('src','smolgrey.jpeg');
+  image.setAttribute('src','smolgrey.jpg');
   document.body.appendChild(image);
-  alert `Your cat is ready ${image}`
+  resultsContainer.innerHTML = `Hi, ${getFormData()} Your cat is ready ${image}`
 } else if (result[0] === 'White' && result[1] === 'Smol') {
   var image = document.createElement('img');
-  image.setAttribute('src','smolwhite.jpeg');
+  image.setAttribute('src','smolwhite.jpg');
   document.body.appendChild(image);
-  alert `Your cat is ready ${image}`
+  resultsContainer.innerHTML = `Hi, ${getFormData()} Your cat is ready ${image}`
 }  else if (result[0] === 'White' && result[1] === 'Chonky') {
   var image = document.createElement('img');
-  image.setAttribute('src','chonkywhite.jpeg');
+  image.setAttribute('src','chonkywhite.jpg');
   document.body.appendChild(image);
-  alert `Your cat is ready ${image}`
+  resultsContainer.innerHTML = `Hi, ${getFormData()} Your cat is ready ${image}`
 }
 }
+
+catQuiz()
+submitButton.addEventListener('click', catQuiz, getFormData);
